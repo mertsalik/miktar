fd0c1fc47ced74c2995f918cc83835e45b3075a9
mertsalik_akinon <mert.salik@akinon.com>
2019-01-07 18:49:03 +0300

    camel case

--- index.js
+++ index.js
@@ -52,16 +52,16 @@ exports.amount2Text = function (amount) {
         return "";
     }
     // split the fractional part
-    var fractional_part = amountInString.split(".")[1]
+    var fractionalPart = amountInString.split(".")[1]
     amountInString = amountInString.split(".")[0]
-    if (fractional_part != undefined){
+    if (fractionalPart !== undefined){
 
-        if (fractional_part.length == 1){
+        if (fractionalPart.length === 1){
             // fill right part with zero.
-            fractional_part = fractional_part + "0";
+            fractionalPart = fractionalPart + "0";
         }
-        else if (fractional_part.length >= 2){
-            fractional_part = fractional_part.slice(0, 2);
+        else if (fractionalPart.length >= 2){
+            fractionalPart = fractionalPart.slice(0, 2);
         }
         else{
             // bug or input like "127." or "5."
